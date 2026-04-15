38b462957bd4e30d3cffdcd036bd74f1b909fb5a
eundongg <remy08@naver.com>
2026-04-15 23:51:02 +0900

    feat: 상품 추가

--- frontend/src/constants/productImageMap.js
+++ frontend/src/constants/productImageMap.js
@@ -2,4 +2,6 @@ export const PRODUCT_IMAGE_MAP = {
   "매경e신문": "/image/매경e신문.png",
   "매경이코노미": "/image/매경이코노미.png",
   "매경럭스멘": "/image/매경럭스멘.png",
+  "지면신문": "/image/지면신문.png",
+  "지면신문(대학생 전용)": "/image/지면신문(대학생 전용).png",
 };
